b396c16ba121c09f4ce5b64789dc260cdf7944a5
TTsham6 <37510144+TTsham6@users.noreply.github.com>
2025-04-13 00:43:32 +0900

    domain name取得

--- 100masu-trainer-aws/index.ts
+++ 100masu-trainer-aws/index.ts
@@ -1,9 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as s3 from "./aws/s3";
 
+const config = new pulumi.Config();
+
 // WEB site
 const webS3 = new s3.S3bucket("masu-web", {
-  bucketName: "masu-web",
+  domainName: config.require("domainName"),
   siteDir: "./app/web/dist",
 });
 
