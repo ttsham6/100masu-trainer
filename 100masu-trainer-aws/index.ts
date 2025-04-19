34b2e4a2a3b9097b7e55ce790ff3d6f9e9127ff6
ttsham6 <37510144+ttsham6@users.noreply.github.com>
2025-04-19 22:42:32 +0900

    ドメイン設定を追加 (#23)
    * s3更新
    
    * domain name取得
    
    * Route53を追加
    
    * 不要なimportを削除

--- 100masu-trainer-aws/index.ts
+++ 100masu-trainer-aws/index.ts
@@ -1,10 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as s3 from "./aws/s3";
+import * as route53 from "./aws/route53";
+import * as aws from "@pulumi/aws";
+
+const config = new pulumi.Config();
 
 // WEB site
 const webS3 = new s3.S3bucket("masu-web", {
-  bucketName: "masu-web",
+  domainName: config.require("domainName"),
   siteDir: "./app/web/dist",
 });
 
-exports.webUrl = webS3.bucket.websiteEndpoint;
+const usEast1Provider = new aws.Provider("us-east-1", {
+  region: "us-east-1",
+});
+
+// Route53
+const route53service = new route53.Route53("masu-web", {
+  domainName: config.require("domainName"),
+  usEast1Provider,
+});
